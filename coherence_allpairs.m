eb15f8185e153de6685ae4ca7f1cb8ea87b18983
Bryn Crandles <64491479+bryncrandles@users.noreply.github.com>
2020-05-11 15:05:59 -0400

    Add existing files

--- coherence_allpairs.m
+++ coherence_allpairs.m
@@ -1,42 +1,51 @@
-% variables
+%% variables
 srate = ALLEEG.srate;
 numchannels = ALLEEG.nbchan;
 numwindows = floor(ALLEEG.pnts/ALLEEG.srate);
 data = EEG.data;
 
+%% pairs of channels
 % number of pairs of channels
 numpairs = nchoosek(numchannels, 2);
-
 % vector of each pair of channels
 pairs = nchoosek(1:numchannels, 2);
 
-% create matrices of zeros to store data 
-PSD1 = zeros(numwindows*numchannels, srate);
-PSD2 = zeros(numwindows*numchannels, srate);
-PSD11 = zeros(numchannels, srate);
-PSD21 = zeros(numchannels, srate);
-CSD = zeros(3*numwindows, srate);
-CSD1 = zeros(numchannels, srate);
-COH = zeros(numchannels, srate);
+%% create matrices of zeros to store data 
+PSD1 = zeros(numpairs, srate);
+PSD2 = zeros(numpairs, srate);
+CSD = zeros(numpairs, srate);
+COH = zeros(numpairs, srate);
 
-% loop to calculate coherence of each pair of channels
- for i = 1:3
+%% loop to calculate sum of PSD of windows for each channel 
+% and sum of CSD of windows for each pair of channels
+for i = 1:numpairs
     for j = 1:numwindows
-        PSD1(numwindows*(i-1) + j, :) = abs((fft(data(pairs(i, 1), 1 + srate*(j-1) : srate*j)))).^2;
-        PSD2(numwindows*(i-1) + j, :) = abs((fft(data(pairs(i, 2), 1 + srate*(j-1) : srate*j)))).^2;
-        CSD(numwindows*(i-1) + j, :) = fft(data(pairs(i, 1), (1 + srate*(j-1) : srate*j))).*conj(fft(data(pairs(i, 2), (1 + srate*(j-1) : srate*j))));
-        PSD11(i, :) = mean(PSD1(numwindows*(i-1) + 1:numwindows*i, :));
-        PSD21(i, :) = mean(PSD2(numwindows*(i-1) + 1:numwindows*i, :));
-        CSD1(i, :) = mean(CSD(numwindows*(i-1) + 1:numwindows*i, :));
-        COH(i, :) = ((abs(CSD1(i, :))).^2)./((PSD11(i, :)).*(PSD21(i, :)));
+        PSD1(i, :) = PSD1(i, :) + (abs(fft(data(pairs(i, 1), 1+ srate*(j-1):srate*j)))).^2;
+        PSD2(i, :) = PSD2(i, :) + (abs(fft(data(pairs(i, 2), 1+ srate*(j-1):srate*j)))).^2;
+        CSD(i, :) = CSD(i, :) + fft(data(pairs(i, 1), (1 + srate*(j-1) : srate*j))).*conj(fft(data(pairs(i, 2), (1 + srate*(j-1) : srate*j))));
     end
 end
-% this works but the matrix CSD is too big for matlab to calculate CSD of all pairs. 
 
+%% loop to calculate mean PSD's and CSD's
+for i = 1:numpairs
+    PSD1(i, :) = PSD1(i, :)./numwindows;
+    PSD2(i, :) = PSD2(i, :)./numwindows;
+    CSD(i, :) = CSD(i, :)./numwindows;
+end
 
+%% calculate coherence of each pair
+% loop to calculate coherence of each pair
+for i = 1:numpairs
+    COH(i, :) = ((abs(CSD(i, :))).^2)./(PSD1(i, :).*(PSD2(i, :)));
+end
 
-
-
-    
-
-
+%% plot data
+% rearrange coherence vectors- get rid of negative frequencies
+COH1 = COH(:, 1:srate/2);
+% frequency vector
+freq = linspace(0, srate/2, srate/2);
+figure()
+hold on
+for i = 1:numpairs
+    plot(freq, COH1(i, :));
+end 
