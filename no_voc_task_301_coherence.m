7bedd004f3fe4ede4974fc134ca3393c37ada0e9
Bryn Crandles <64491479+bryncrandles@users.noreply.github.com>
2020-05-29 17:07:10 -0400

    Add existing file

---
+++ no_voc_task_301_coherence.m
@@ -0,0 +1,19 @@
+% compute and save the coherence matrix for each pair of channels from the
+% no vocal task 301 data set
+
+% load eeglab 
+eeglab
+
+% import data set
+EEG = pop_loadset('sub-301_sz_eeg_clean.set')
+
+% variables 
+data = EEG.data; 
+srate = EEG.srate;
+brkpnts = [0 EEG.pnts];
+
+% compute coherence
+no_voc_task_301_coh = coh(data, srate, brkpnts);
+
+% save coherence 
+save('no_voc_task_301_coherence.mat', 'no_voc_task_301_coh') 
